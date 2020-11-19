29b1d80545f75f883f31271ab2beecaa813304ef
flnnhuman <peseckiy.vlad@gmail.com>
2020-11-19 13:33:51 +0300

    first steam login button

---
+++ Roulette/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roulette.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult RedirectToLogin()
+        {
+            return RedirectPermanent("/Login");
+        }
+
+        public IActionResult  Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Roulette/Controllers/SteamLoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web;
+using AngleSharp.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roulette.Controllers
+{
+    public class SteamLoginController : Controller
+    {
+        [HttpGet("~/loginView")]
+        public IActionResult LoginView()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("~/login")]
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties {RedirectUri = "/"}, "Steam");
+        }
+        
+        [HttpGet("~/signin-steam")]
+        public IActionResult AfterLoginRedirection()
+        {
+            string steamId = HttpUtility
+                .ParseQueryString(Request.QueryString.ToString())
+                .Get("openid.identity")
+                .Replace("https://steamcommunity.com/openid/id/", "");
+
+            var cookieOptions = new CookieOptions {Expires = new DateTimeOffset(DateTime.Now.AddDays(1))};
+
+            HttpContext.Response.Cookies.Append("SteamId", steamId, cookieOptions);
+
+            return RedirectPermanent("/");
+        }
+
+        [HttpPost("~/steamLogout")]
+        public IActionResult SignOut()
+        {
+            HttpContext.Response.Cookies.Append("SteamId", "");
+            return RedirectPermanent("/");
+            //return SignOut(new AuthenticationProperties { RedirectUri = "/" }, CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+       
+    }
+}
