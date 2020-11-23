972a1965a3af91b7009082ed8fe79290d257becc
flnnhuman <peseckiy.vlad@gmail.com>
2020-11-23 21:20:41 +0300

    added easing of spin and new timer animation

--- Roulette/wwwroot/js/ScriptLoader.js
+++ Roulette/wwwroot/js/ScriptLoader.js
@@ -10,18 +10,39 @@
 };
 
 
-window.Animate = (currentPos, numWidth,moves) => {
+window.Animate = (currentPos, numWidth, moves, t) => {
 
     console.log("curpos = " + currentPos);
     console.log("numWidth = " + numWidth);
     console.log("moves = " + moves);
     $('#case').animate({
         "background-position": currentPos - 2100 - (moves * numWidth),
-    }, 3000);
+    }, {
+        'duration': t * 1000,
+        'easing': $.easing.easeOutExpo(),
+
+    });
 };
 
 
 window.Resize = () =>{
     console.log("resized")
     resize();
+}
+
+
+window.Bar = (t) => {
+    $(".timer").finish().css("width", "100%");
+    _is_rolling = false;
+    end_timer = Math.floor(new Date() / 1000) + t;
+    $(".timer").animate({
+        'width': '0%'
+    }, {
+        'duration': t * 1000,
+        'easing': 'linear',
+        // 'progress': function (t, e, a) {
+        //     $BANNER.html(LNG.ROLL_TIME.replace('%time%', (a / 1000).toFixed(2)));
+        // },
+
+    });
 }
