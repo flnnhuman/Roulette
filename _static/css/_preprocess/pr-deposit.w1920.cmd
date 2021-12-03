b3f2b3e8c37fd8498429f2e81bf78168f1609b0d
gotSpeed <am.moving.lightspeed@gmail.com>
2021-12-04 02:35:46 +0300

    Fixed trade bag items / moved common styles to a separate file.

--- _static/css/_preprocess/pr-deposit.w1920.cmd
+++ _static/css/_preprocess/pr-deposit.w1920.cmd
@@ -10,3 +10,9 @@ SET targ=%prefix%\deposit.out.css
 SET var=%prefix%\var.txt
 
 python %proc% -s %src% -t %targ% -v %var%
+
+SET src=%prefix%\deposit.common.css
+SET targ=%prefix%\deposit.common.out.css
+SET var=%prefix%\var.txt
+
+python %proc% -s %src% -t %targ% -v %var%
