fad11971040fe9e0e496364cb0fadeb152a987ed
Angelina Cherkas <a.cherkas@gmail.com>
2021-12-04 17:35:40 +0300

    recreate migrations

---
+++ Roulette/Controllers/HistoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Roulette.Context;
+using Roulette.Models;
+
+namespace Roulette.Controllers
+{
+	public class HistoryController : Controller
+	{
+		private readonly AppDbContext AppDbContext;
+		public HistoryController(AppDbContext context)
+		{
+			AppDbContext = context;
+		}
+
+		public List<GameModel> GetLastGames(int page, int size)
+		{
+			if (page < 0)
+			{
+				return new List<GameModel>();
+			}
+			return AppDbContext.GamesHistory.AsEnumerable().Skip(20 * page).Take(20).ToList();
+		}
+		public GameModel GetGame(long id)
+		{
+			return AppDbContext.GamesHistory.Find(id);
+		}
+	}
+}
