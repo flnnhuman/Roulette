f6576af4f86496ea7def8a386c4d03bddbb8adc1
gotSpeed <am.moving.lightspeed@gmail.com>
2021-12-05 01:59:15 +0300

    Added UI-HISTORY-SNAPSHOT-MODAL.

--- _static/_preprocess/pr-hist-snap.cmd
+++ _static/_preprocess/pr-hist-snap.cmd
@@ -1,12 +1,14 @@
 SET base_dir=D:\Kabanina\7\SDT\Roulette
 
-CD %base_dir%
+cd %base_dir%
 
 SET proc=%base_dir%\_static\preprocess.py
-SET prefix=_static\css\deposit\w1920
 
-SET src=%prefix%\deposit.css
-SET targ=%prefix%\deposit.out.css
+
+SET prefix=_static\css\deposit
+
+SET src=%prefix%\pr-hist-snap.css
+SET targ=%prefix%\pr-hist-snap.out.css
 SET var=%prefix%\var.txt
 
 python %proc% -s %src% -t %targ% -v %var%
